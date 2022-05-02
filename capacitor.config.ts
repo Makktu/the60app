04fe65e4c0a9de100e96a0ea3d0122614fd5e5e1
Makktu <johnmcna@gmail.com>
2022-05-02 16:50:27 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'the60app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
